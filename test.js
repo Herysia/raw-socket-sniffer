3264cdd1e3534c337644fe4eba3df1f954df1b96
Herysia <totogirerd@gmail.com>
2023-01-07 17:19:58 +0100

    Add test script

--- test.js
+++ test.js
@@ -1,31 +1,49 @@
-"use strict";
-const { EventEmitter } = require("events");
-const { inherits } = require("util");
+const { RawSocket } = require("./index.js");
+const Cap = require("cap").Cap;
+var decoders = require("cap").decoders;
+var PROTOCOL = decoders.PROTOCOL;
 
-const { RawSocketCapture } = require("bindings")("addon");
+let rsCount = 0;
+let rsLen = 0;
+const port = 443;
+const rs = new RawSocket("192.168.0.51", port);
+rs.on("data", (data) => {
+  const eth = decoders.Ethernet(data);
+  if (eth.info.type === PROTOCOL.ETHERNET.IPV4) {
+    const ip = decoders.IPV4(data, eth.offset);
+    if (ip.info.protocol === PROTOCOL.IP.TCP || ip.info.protocol === PROTOCOL.IP.UDP) {
+      rsLen += ip.info.totallen;
+      rsCount++;
+      console.log(`Rs: ${ip.info.totallen} - ${rsLen}(${rsCount})`);
+    }
+  }
+});
+const c = new Cap();
+const device = Cap.findDevice("192.168.0.51");
+const filter = `(tcp or udp) and (dst port ${port} or src port ${port})`;
+const bufSize = 10 * 1024 * 1024;
+const buffer = Buffer.alloc(65535);
+const capData = { c, buffer, device, len: 0, count: 0 };
 
-inherits(RawSocketCapture, EventEmitter);
-class RawSocket extends EventEmitter {
-  constructor(ip, port) {
-    super();
-    this.binding = new RawSocketCapture(ip, port);
-    this.binding.on("data", (data) => {
-      this.emit("data", data);
-    });
+const linkType = c.open(device, filter, bufSize, buffer);
+
+c.setMinBytes && c.setMinBytes(0);
+
+rs.listen();
+c.on("packet", function (nbytes, trunc) {
+  if (linkType === "ETHERNET") {
+    const eth = decoders.Ethernet(buffer);
+
+    if (eth.info.type === PROTOCOL.ETHERNET.IPV4) {
+      const ip = decoders.IPV4(buffer, eth.offset);
+      if (ip.info.protocol === PROTOCOL.IP.TCP || ip.info.protocol === PROTOCOL.IP.UDP) {
+        capData.len += ip.info.totallen;
+        capData.count++;
+      }
+      console.log(`Cap: ${ip.info.totallen} - ${capData.len}(${capData.count})`);
+    }
   }
-  listen() {
-    this.binding.listen();
-  }
-}
-const rsc = new RawSocket("192.168.0.51", 0);
-rsc.on("data", (data) => {
-  console.log(data.subarray(0, 14).toString("hex"));
 });
-rsc.listen();
-/*
-const rsc2 = new RawSocket("172.19.192.1", 0);
-rsc2.on("data", (data) => {
-  console.log(2, data.length);
-});
-rsc2.listen();
-*/
+setInterval(() => {
+  console.log(`cap/socket : ${capData.len}(${capData.count})/${rsLen}(${rsCount})`);
+}, 1000);
